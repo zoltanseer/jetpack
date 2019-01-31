3d465e0fa282b6ad2116f203f688b4682848f8ef
Jason Moon <jason.moon@automattic.com>
2019-01-31 13:25:15 -0700

    Visited: Add criteria and threshold attribute handling

--- modules/blocks.php
+++ modules/blocks.php
@@ -171,5 +171,16 @@ jetpack_register_block(
  */
 function jetpack_visited_block_render( $attributes, $content ) {
 	Jetpack_Gutenberg::load_assets_as_required( 'visited' );
-	return $content;
+
+	$count = intval( $_COOKIE[ 'wp-visit-tracking' ] );
+	$criteria = $attributes['criteria'] ?? null;
+	$threshold = intval( $attributes['threshold'] ) ?? 0;
+
+	if (
+		( $criteria === 'after-visits' && $count >= $threshold ) ||
+		( $criteria === 'before-visits' && $count <= $threshold )
+	) {
+		return $content;
+	}
+	return null;
 }
