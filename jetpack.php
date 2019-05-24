a1be0518a2c356af0f1eb4a82b0859b36321fb1c
Michael Turk <turk.michael@gmail.com>
2019-05-24 11:38:36 -0400

    Ensure the autoloader error message function is only defined once for plugins in the Jetpack ecosystem

--- jetpack.php
+++ jetpack.php
@@ -133,20 +133,22 @@ function jetpack_admin_unsupported_php_notice() { ?>
  *
  * @since 7.4.0
  */
-function jetpack_admin_missing_autoloader() { ?>
-	<div class="notice notice-error is-dismissible">
-		<p>
+if ( ! function_exists( 'jetpack_admin_missing_autoloader' ) ) {
+	function jetpack_admin_missing_autoloader() { ?>
+		<div class="notice notice-error is-dismissible">
+			<p>
+				<?php
+				printf(
+				/* translators: Placeholder is a link to a support document. */
+					__( 'Your installation of Jetpack is incomplete. If you installed Jetpack from GitHub, please refer to <a href="%1$s" target="_blank" rel="noopener noreferrer">this document</a> to set up your development environment.', 'jetpack' ),
+					esc_url( 'https://github.com/Automattic/jetpack/blob/master/docs/development-environment.md' )
+				);
+				?>
+			</p>
+			</p>
+		</div>
 		<?php
-		printf(
-			/* translators: Placeholder is a link to a support document. */
-			__( 'Your installation of Jetpack is incomplete. If you installed Jetpack from GitHub, please refer to <a href="%1$s" target="_blank" rel="noopener noreferrer">this document</a> to set up your development environment.', 'jetpack' ),
-			esc_url( 'https://github.com/Automattic/jetpack/blob/master/docs/development-environment.md' )
-		);
-		?>
-		</p>
-	</p>
-	</div>
-	<?php
+	}
 }
 
 if ( version_compare( phpversion(), JETPACK__MINIMUM_PHP_VERSION, '<' ) ) {
