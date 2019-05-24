cd3678a6ba40bbb7cd9559a82554d456c97f9bb8
Brandon Kraft <public@brandonkraft.com>
2019-05-24 11:33:31 -0500

    Tagging 2.2.2

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://wordpress.org/extend/plugins/jetpack/
  * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
  * Author: Automattic
- * Version: 2.2.1
+ * Version: 2.2.2
  * Author URI: http://jetpack.me
  * License: GPL2+
  * Text Domain: jetpack
@@ -17,7 +17,7 @@ define( 'JETPACK__API_VERSION', 1 );
 define( 'JETPACK__MINIMUM_WP_VERSION', '3.3' );
 defined( 'JETPACK_CLIENT__AUTH_LOCATION' ) or define( 'JETPACK_CLIENT__AUTH_LOCATION', 'header' );
 defined( 'JETPACK_CLIENT__HTTPS' ) or define( 'JETPACK_CLIENT__HTTPS', 'AUTO' );
-define( 'JETPACK__VERSION', '2.2.1' );
+define( 'JETPACK__VERSION', '2.2.2' );
 define( 'JETPACK__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 defined( 'JETPACK__GLOTPRESS_LOCALES_PATH' ) or define( 'JETPACK__GLOTPRESS_LOCALES_PATH', JETPACK__PLUGIN_DIR . 'locales.php' );
 
@@ -1038,7 +1038,16 @@ class Jetpack {
 			if ( $module_data['requires_connection'] ) {
 				return false;
 			}
+			$active_state[] = $module;
+			Jetpack::state( $state, implode( ',', $active_state ) );
+			Jetpack::update_option( 'active_modules', array_unique( $active ) );
+			ob_end_clean();
 		}
+		Jetpack::state( 'error', false );
+		Jetpack::state( 'module', false );
+		Jetpack::catch_errors( false );
+		do_action( 'jetpack_activate_default_modules', $min_version, $max_version, $other_modules );
+	}
 
 		// Check and see if the old plugin is active
 		if ( isset( $jetpack->plugins_to_deactivate[$module] ) ) {
